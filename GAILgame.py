41d6cc025e8218b668df04933cf6e2102e115788
Olga <petroolg@fel.cvut.cz>
2018-05-18 12:41:00 +0200

    Create fnc for occupancy measure.

--- GAILgame.py
+++ GAILgame.py
@@ -2,6 +2,12 @@ import tensorflow as tf
 import numpy as np
 import os
 import shutil
+import logging
+from scipy import misc
+import sklearn
+from sklearn import preprocessing
+from PythonClient.AirSimClient import *
+import subprocess, signal
 
 class CNN:
 
@@ -163,7 +169,6 @@ class CNN:
                 h_pool =  tf.nn.max_pool(h_conv1,
                                           ksize=pool_ksize,
                                           strides=pool_strides,
-
                                           padding='SAME')
             # save output of conv layer to TensorBoard - first 16 filters
             with tf.name_scope('Image_output_'+global_scope+'_'+scope):
@@ -278,38 +283,173 @@ def measure_perf(expert_traj, model, agent_trajs):
     graphs[0].append(agent_score)
     graphs[1].append(expert_score)
 
+def create_traj(paths):
+    # Timestamp  Speed (kmph)  Throttle  Steering  Brake  Gear  ImageName
+    trajectories = []
+    for path in paths:
+        traj = []
+        with open(path+'\\airsim_rec.txt', 'r') as file:
+            file.readline() # get rid of header
+            for i, line in enumerate(file):
+                traj.append(line.split('\t'))
+                imgs = traj[i][-1].split(';')
+                traj[i].pop(-1)
+                traj[i] = [float(x) for x in traj[i][:-1]]
+                traj[i] = traj[i] + [img.strip() for img in imgs]
+        trajectories.append(traj)
+    print(traj)
+    return np.array(trajectories)
+
+def action_hash(action):
+    # action: Throttle   Steering        Brake
+    #         [0, 1]     int([-1, 1])    [0, 1]
+    action = np.atleast_2d(action).astype(float).astype(int)
+
+    hashes = action[:, 0] + action[:,2]*2 + (action[:,1]+1)*4
+
+    return hashes
+
+def img_hash(expert_trajectory_path, img_path):
+
+    hashes = []
+
+    for path in img_path:
+        img = misc.imread(expert_trajectory_path+'\\images\\'+path)
+        img = preprocessing.normalize(img[0])
+
+        bits = "".join(map(lambda pixel: '1' if pixel < 0.5 else '0', img.ravel()))
+        hexadecimal = int(bits, 2)
+        hashes.append(hexadecimal)
+
+    return hashes
+
+def state_hash(path, state):
+    # state: Speed (kmph)  Throttle  Steering  Brake  Gear  Image1 Image2
+
+    action = np.atleast_2d(state)
+    bins = np.linspace(-1, 1, 21)
+
+    hashes = [hash(stsb) for stsb in zip(state[:, 0],
+                                         state[:, 4],
+                                         img_hash(path, state[:, 5]),
+                                         img_hash(path,  state[:, 7]))]
+    hashes = np.array(hashes) / 12
+    return hashes
+
+
+def sa_set_construct(expert_trajectory_paths, trajs):
+
+    sa_dict = dict()
+    sa_set = set()
+    for traj, path in zip(trajs, expert_trajectory_paths):
+
+        states = state_hash(path, traj)
+        actions = action_hash(traj[:,2:5])
+
+        for s, a in zip(states, actions):
+            acts = sa_dict.get(s, set())
+            acts = acts.union(set([a]))
+            sa_dict[s] = acts
+
+        sa_set = sa_set.union(set(states+actions))
+
+    return list(sa_set), sa_dict.keys(), sa_dict.values()
+
+def occupancy_measure(expert_trajectory_paths, trajs):
+
+    # construct set of all state-action pairs
+    # hash codes for s-a pair: [state hash/12 + action hash]
+
+    sa_set, s_set, a_sets = sa_set_construct(expert_trajectory_paths, trajs)
+
+    om = dict()
+    max_d = max([len(t) for t in trajs])
+    om.update(dict(zip(sa_set, np.zeros((len(sa_set), max_d)))))
+
+    for t, path in zip(trajs, expert_trajectory_paths):
+        # TODO: convert states to hash codes
+        states = state_hash(path, t)
+        actions = action_hash(t[:,2:5])
+
+        vals = np.array([om.get(key) for key in states+actions])
+        vals[range(len(t)), range(len(t))] += 1
+        for sa, val in zip(states+actions, vals):
+            om[sa] += val
+
+    GAMMA = 0.9
+    # TODO: compute actual probabilities
+    out = dict()
+    for state, actions in zip(s_set, a_sets):
+        state_action_pairs = np.array([om.get(action+state) for action in actions])
+        pi = np.sum(state_action_pairs,axis=1)/np.sum(state_action_pairs)
+        P = np.nan_to_num(np.divide(state_action_pairs, np.sum(state_action_pairs, axis=0)),0)
+        probs = [np.sum([s*GAMMA**i for i, s in enumerate(st)]) for st in P]
+        out.update(dict(zip([action+state for action in actions], pi*probs)))
+    print('\n'.join([str(k) for k in list(out.values())]))
+    return out
+
+def sample_trajectories(client, n_tries=30):
+
+    # launch simulator
+    os.system("C:/Users/Olga/Documents/CityEnviron/CityEnviron/CityEnviron.exe")
+
+    client = CarClient()
+    client.confirmConnection()
+    client.enableApiControl(True)
+    car_controls = CarControls()
+
+    for i in range(n_tries):
+
+
+
+        # TODO: throttle till full stop
+
+    # restore to original state
+    client.reset()
+    client.enableApiControl(False)
+
+    # kill simulator process
+    p = subprocess.Popen(['tasklist'], stdout=subprocess.PIPE)
+
+    out, err = p.communicate()
+
+    for line in out.splitlines():
+        if 'CityEnviron' in line:
+            pid = int(line.split(None, 1)[0])
+            os.kill(pid, signal.SIGKILL)
+            break
+
+
 if __name__== '__main__':
 
-    expert_trajectory_path = 'C:\\Users\\Olga\\Documents\\AirSim\\2018-04-13-11-46-30'
-    game = gailGame()
-    print('Initialization completed')
+    # expert_trajectory_paths = ['C:\\Users\\Olga\\Documents\\AirSim\\2018-04-13-11-46-30']
+    # # game = gailGame()
+    # # print('Initialization completed')
+    #
+    # expert_traj = create_traj(expert_trajectory_paths)
+    # # # eOM = occ_measure(expert_traj)
+    # eOM = occupancy_measure(expert_trajectory_paths, expert_traj)
 
-    expert_traj = np.array(expert_traj)
-    # eOM = occ_measure(expert_traj)
-    eOM = occupancy_measure_approx_vector(expert_traj)
-    show_om(eOM)
-
-    delete_imgs('images/')
-    logging.basicConfig(filename='images/GAILgame.log', level=logging.DEBUG)
+    # logging.basicConfig(filename='images/GAILgame.log', level=logging.DEBUG)
 
     # TODO: connect to simulator
-    T = 10e4
-    for i in range(T):
-        print('{}/{}'.format(T,i))
-        logging.debug('======Iteration #{}======'.format(i))
 
-        agent_trajs = sample_trajectories(game, model)
-
-        # Update the Discriminator parameters from wi to wi+1 with the gradient
-        OM = occupancy_measure_approx_vector(agent_trajs)
-        model.partial_fit_D(OM, eOM)
-
-        if i%10 == 0:
-            plot_graphs('images/stat_graph.png')
-
-        # Take a policy step from θi to θi+1, using the TRPO rule with cost function log(Dwi+1 (s, a)).
-        QOM = occ_meaure_Q(agent_trajs)
-        model.partial_fit_policy(OM, QOM)
-
-        measure_performance(expert_traj, model, agent_trajs)
-
+    # T = 10e4
+    # for i in range(T):
+    #     print('{}/{}'.format(T,i))
+    #     logging.debug('======Iteration #{}======'.format(i))
+    #
+    #     agent_trajs = sample_trajectories(model)
+    #
+    #     # Update the Discriminator parameters from wi to wi+1 with the gradient
+    #     OM = occupancy_measure_approx_vector(agent_trajs)
+    #     model.partial_fit_D(OM, eOM)
+    #
+    #     if i%10 == 0:
+    #         plot_graphs('images/stat_graph.png')
+    #
+    #     # Take a policy step from θi to θi+1, using the TRPO rule with cost function log(Dwi+1 (s, a)).
+    #     QOM = occ_meaure_Q(agent_trajs)
+    #     model.partial_fit_policy(OM, QOM)
+    #
+    #     measure_performance(expert_traj, model, agent_trajs)
